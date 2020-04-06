f33ec5d01d6d7d1b010b2c04214f0dfdd6777ad0
augustineaykara <augustineaykara@gmail.com>
2020-04-06 13:07:02 +0530

    upload via git

--- djangoTodoApp/accounts/admin.py
+++ djangoTodoApp/accounts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import userTodo
 
 # Register your models here.
+
+admin.site.register(userTodo)
--- djangoTodoApp/accounts/models.py
+++ djangoTodoApp/accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class userTodo(models.Model):
+    userName = models.ForeignKey(User, on_delete = models.CASCADE)
+    todoTitle = models.CharField(max_length = 100)
+    todoDescription = models.TextField()
